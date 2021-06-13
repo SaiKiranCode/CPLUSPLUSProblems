d583f1a9e66e3c8158ba909bb275da7ccc62fc8a
saikiran20002102 <61832514+saikiran20002102@users.noreply.github.com>
2021-06-13 17:08:42 +0530

    Add files via upload

---
+++ c++LabProblems/set6/set6chanq1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int fill_array(double *name,int size)
+{
+	name= new double[size];
+	cout<<"enter the elements in array"<<endl;
+	for(int i=0;i<size;i++)
+	{
+		
+
+
+	}
+
+
+
+
+
+
+
+}
+
+
+
+
+int main()
+{
+
+
+
+	return 0;
+}
